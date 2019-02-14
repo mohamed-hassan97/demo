687c31e19c1fac748b75be49aadb61a747074343
Mohamed <Mohamed@mohamed.local>
2019-02-14 12:16:43 +0300

    Value form and import

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,7 +7,7 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Controller\SecurityController;
